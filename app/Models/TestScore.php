40aa4cddef64487cce36c29b4ff94deaf6d58d54
viplucmca@yahoo.co.in <viplucmca@yahoo.co.in>
2026-01-29 18:04:40 +0530

    Fix test score issue

--- app/Models/TestScore.php
+++ app/Models/TestScore.php
@@ -6,6 +6,17 @@ use Kyslik\ColumnSortable\Sortable;
 use Illuminate\Database\Eloquent\Model;
 
 class TestScore extends Model
-{	use Sortable;
+{
+    use Sortable;
 
+    protected $fillable = [
+        'user_id',
+        'client_id',
+        'type',
+        'toefl_Listening', 'toefl_Reading', 'toefl_Writing', 'toefl_Speaking', 'toefl_Date',
+        'ilets_Listening', 'ilets_Reading', 'ilets_Writing', 'ilets_Speaking', 'ilets_Date',
+        'pte_Listening', 'pte_Reading', 'pte_Writing', 'pte_Speaking', 'pte_Date',
+        'score_1', 'score_2', 'score_3',
+        'sat_i', 'sat_ii', 'gre', 'gmat',
+    ];
 }
