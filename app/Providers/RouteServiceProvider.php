d33875f68c6fa8ccfa9d985bdaa75fd1e0966fb3
bansallawyers12 <bansallawyers12@gmail.com>
2026-06-05 13:21:49 +1000

    Register elite routes in RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,6 +30,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace('App\Models\Http\Controllers')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/elite.php'));
         });
     }
 }
